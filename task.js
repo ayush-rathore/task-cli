c2a839f2f981dcdb2b4f7b327250a5377f384a8d
ayush-rathore <aayushh79@gmail.com>
2021-12-22 20:10:41 +0530

    Update version

--- task.js
+++ task.js
@@ -1,5 +1,5 @@
 // Author - Ayush Rathore
-// Version - 1.0
+// Version - 1.5
 
 const fs = require("fs"); // Import the fileSystem module
 const process = require("process"); // Import the process module
