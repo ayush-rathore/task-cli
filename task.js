0250b19e7f3275d89d58681e86784d354e52c375
Ayush Rathore <aayushh79@gmail.com>
2022-01-09 19:56:00 +0530

    Fix garbage statements on console
    Caused by colors module

--- task.js
+++ task.js
@@ -4,7 +4,12 @@
 const fs = require("fs"); // Import the fileSystem module
 const process = require("process"); // Import the process module
 
-const colors = require("colors"); // Import the colors module
+const clc = require("cli-color"); // Cli-Color Module
+
+const green = clc.green;
+const red = clc.red;
+const yellow = clc.yellow;
+const blue = clc.blue;
 
 let args = process.argv; // Get the arguments from the command line
 
@@ -13,28 +18,33 @@ let cmd = args[2]; // Get the command
 // Usage or Help command
 const help = () => {
 	console.log(
-		`		|----------------------------------------------------------------|`
-			.green
+		green(
+			`		|----------------------------------------------------------------|`
+		)
 	);
 	console.log(
-		`		|                    Welcome to Task CLI                      	 |`.green
+		green(
+			`		|                    Welcome to Task CLI                      	 |`
+		)
 	);
 	console.log(
-		`		|  A command-line (CLI) program that lets you manage your tasks. |`
-			.green
+		green(
+			`		|  A command-line (CLI) program that lets you manage your tasks. |`
+		)
 	);
 	console.log(
-		`		|----------------------------------------------------------------|`
-			.green
+		green(
+			`		|----------------------------------------------------------------|`
+		)
 	);
 	console.log(
-		`Usage :-
+		yellow(`Usage :-
 $ ./task add 2 "hello world"  # Add a new task with priority 2 and text "hello world" to the list
 $ ./task ls                   # Show incomplete priority list tasks sorted by priority in ascending order
 $ ./task del INDEX            # Delete the incomplete task with the given index
 $ ./task done INDEX           # Mark the incomplete task with the given index as complete
 $ ./task help                 # Show usage
-$ ./task report               # Statistics`.yellow
+$ ./task report               # Statistics`)
 	);
 };
 
@@ -43,12 +53,12 @@ const add = () => {
 	let priority = args[3];
 	let cmdStatement = args[4];
 	if (priority == undefined || cmdStatement == undefined) {
-		console.log("Error: Missing tasks string. Nothing added!".red);
+		console.log(red("Error: Missing tasks string. Nothing added!"));
 	} else {
 		let addStatement = "\n" + `${priority} ${cmdStatement}`;
 		fs.appendFile("task.txt", addStatement, () => {
 			console.log(
-				`Added task: "${cmdStatement}" with priority ${priority}`.green
+				green(`Added task: "${cmdStatement}" with priority ${priority}`)
 			);
 		});
 	}
@@ -58,13 +68,14 @@ const add = () => {
 const del = () => {
 	let index = args[3];
 	if (index == undefined) {
-		console.log("Error: Missing NUMBER for deleting tasks.".red);
+		console.log(red("Error: Missing NUMBER for deleting tasks."));
 	} else {
 		fs.readFile("task.txt", (err, data) => {
 			if (err)
 				console.log(
-					`Error: item with index #${index} does not exist. Nothing deleted.`
-						.red
+					red(
+						`Error: item with index #${index} does not exist. Nothing deleted.`
+					)
 				);
 			else {
 				let lines = data.toString().split("\n");
@@ -72,13 +83,14 @@ const del = () => {
 				if (lines[0] == "") lines.shift();
 				if (index > lines.length || index < 1) {
 					console.log(
-						`Error: item with index #${index} does not exist. Nothing deleted.`
-							.red
+						red(
+							`Error: item with index #${index} does not exist. Nothing deleted.`
+						)
 					);
 				} else {
 					lines.splice(index - 1, 1);
 					fs.writeFile("task.txt", lines.join("\n"), () => {
-						console.log(`Deleted task with index #${index}`.green);
+						console.log(green(`Deleted task with index #${index}`));
 					});
 				}
 			}
@@ -90,12 +102,14 @@ const del = () => {
 const done = () => {
 	let index = args[3];
 	if (index == undefined) {
-		console.log("Error: Missing NUMBER for marking tasks as done.".red);
+		console.log(red("Error: Missing NUMBER for marking tasks as done."));
 	} else {
 		fs.readFile("task.txt", (err, data) => {
 			if (err)
 				console.log(
-					`Error: no incomplete item with index #${index} exists.`.red
+					red(
+						`Error: no incomplete item with index #${index} exists.`
+					)
 				);
 			else {
 				let lines = data.toString().split("\n");
@@ -103,8 +117,9 @@ const done = () => {
 				if (lines[0] == "") lines.shift();
 				if (index > lines.length || index < 1) {
 					console.log(
-						`Error: no incomplete item with index #${index} exists.`
-							.red
+						red(
+							`Error: no incomplete item with index #${index} exists.`
+						)
 					);
 				} else {
 					let complete = lines[index - 1].slice(1).trim();
@@ -115,7 +130,7 @@ const done = () => {
 					});
 					fs.appendFile("completed.txt", "\n" + complete, () => {
 						console.log(
-							`Marked task with index #${index} as done`.green
+							green(`Marked task with index #${index} as done`)
 						);
 					});
 				}
@@ -127,12 +142,12 @@ const done = () => {
 const readTasks = () => {
 	fs.readFile("task.txt", (err, data) => {
 		if (err || data.toString() == "")
-			console.log(`There are no pending tasks!\n`.blue);
+			console.log(blue(`There are no pending tasks!\n`));
 		else {
 			let lines = data.toString().split("\n");
 			lines.sort();
 			if (lines[0] == "") lines.shift();
-			console.log(`Pending : ${lines.length}`.yellow);
+			console.log(yellow(`Pending : ${lines.length}`));
 			for (let i = 0; i < lines.length; i++) {
 				let line = lines[i].split(" ");
 				let priority = line[0];
@@ -147,12 +162,12 @@ const readTasks = () => {
 const readCompleted = () => {
 	fs.readFile("completed.txt", (err, data) => {
 		if (err || data.toString() == "")
-			console.log(`There are no completed tasks!\n`.blue);
+			console.log(blue(`There are no completed tasks!\n`));
 		else {
 			let lines = data.toString().split("\n");
 			if (lines[0] == "") lines.shift();
 			console.log("");
-			console.log(`Completed : ${lines.length}`.green);
+			console.log(yellow(`Completed : ${lines.length}`));
 			for (let i = 0; i < lines.length; i++) {
 				console.log(`${i + 1}. ${lines[i]}`);
 			}
